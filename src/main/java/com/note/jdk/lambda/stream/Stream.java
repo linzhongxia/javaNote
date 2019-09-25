07860aa30de8fa1dafda6c7f68e27a2095e679f4
linzhongxia3 <linzhongxia@jd.com>
2019-09-25 09:50:19 +0800

    stream

--- src/main/java/com/note/jdk/lambda/stream/Stream.java
+++ src/main/java/com/note/jdk/lambda/stream/Stream.java
@@ -5,6 +5,8 @@ import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * @author linzhongxia CMO体系-无界零售赋能事业部-商家研发部-商家营销部
@@ -52,6 +54,18 @@ public class Stream {
 
     }
 
+
+    @Test
+    public void mapTest2(){
+        List<Color> colors = new ArrayList<>();
+        colors.add(new Color("red"));
+        colors.add(new Color("red"));
+
+        Set<String> s = colors.stream().map( color -> color.getColor()).collect(Collectors.toSet());
+
+        System.out.println(JSON.toJSONString(s));
+    }
+
     @Test
     public void streamTest(){
         List<Color> colors = new ArrayList<>();
@@ -75,5 +89,12 @@ public class Stream {
         public void setColor(String color) {
             this.color = color;
         }
+    }
+
+    @Test
+    public  void testNull() {
+        Set<String> set = null;
+
+        set.stream().forEach(s-> {System.out.println(s);});
     }
 }
